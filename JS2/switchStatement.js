9cece14e830049190432bccd32e7da69dfec8d37
ACM <223bhakti0079@dbit.in>
2026-02-08 01:33:25 +0530

    switch statements

---
+++ JS2/switchStatement.js
@@ -0,0 +1,17 @@
+let trafficcolor = "yellow";
+
+switch (trafficcolor){
+    case "red": 
+    console.log("stop");
+    break;
+    case "yellow": 
+    console.log("slow down");
+    break;
+    case "green": 
+    console.log("go");
+    break;
+    default : 
+    console.log("light is broken");
+}
+
+console.log("everything after switch statements can now be continued to be executed")
