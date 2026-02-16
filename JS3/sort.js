17edaaf48bf2a4ae9ab6d41f59fe4135d6a019ae
ACM <223bhakti0079@dbit.in>
2026-02-17 00:58:48 +0530

    .sort() works for character or first letter of a word, for number it converts it into string and then sorts

--- JS3/sort.js
+++ JS3/sort.js
@@ -6,6 +6,6 @@ let words = ["ferrari", "xuv", "toyota", "scorpio", "thar"];
 words.sort();
 console.log(words); //prints string according to first letter alphabetically
 
-let age = ["99", "89", "67", "42", "100"];
+let age = [99, 89, 67, 42, 100];
 age.sort();
 console.log(age); // doesn't work for numbers because it converts numbers to string and then sorts it alphabetically
