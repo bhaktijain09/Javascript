44e1c312584104353968e96df8394f6ae7f845de
ACM <223bhakti0079@dbit.in>
2026-02-21 00:17:51 +0530

    odd numbers in range using for loop (both forward and backward printing)

---
+++ JS4/printOddforLoop.js
@@ -0,0 +1,15 @@
+// print odd numbers from 0 to 10 
+for(let i=1; i<=10; i+=2){ // starting (initialization value is imp) with 1(first odd number in range) and +2 after every iteration gives odd number
+    console.log(i);
+}
+// output - 
+// 1
+// 3
+// 5
+// 7
+// 9
+
+// print odd numbers from 10 to 0 backwards
+for(let i=9; i>=1; i-=2){ // starting (initialization value is imp) with 9(last odd number in range) and -2 after every iteration gives odd number backwards 
+    console.log(i);
+}
