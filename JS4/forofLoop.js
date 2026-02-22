c2d0ad829f9b8d03641f6f8b022d5d320a802baf
ACM <223bhakti0079@dbit.in>
2026-02-23 00:10:26 +0530

    for of loop

---
+++ JS4/forofLoop.js
@@ -0,0 +1,28 @@
+// for of loop - 
+let fruiting = ["apple", "mango", "banana", "litchi", "strawberry"];
+for(fruit of fruiting){
+    console.log(fruit); // prints each fruit of fruits array seperately
+}
+// prints - 
+// apple 
+// mango 
+// banana
+// litchi
+// strawberry
+
+let company = "apna college";
+for(char of company){
+    console.log(char);
+}
+//prints - 
+// a 
+// p 
+// n
+// a 
+
+// c 
+// o 
+// (2) l
+// e 
+// g 
+// e
