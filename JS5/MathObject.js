6ff19ce82e70a68fa6abcfa5b5c0f924c9c97ce9
ACM <223bhakti0079@dbit.in>
2026-03-02 00:05:05 +0530

    Math properties and methods

---
+++ JS5/MathObject.js
@@ -0,0 +1,36 @@
+// // Math properties - 
+// Math.PI            // prints const value of PI = 3.14....
+// Math.E            // prints const value of E = 2.71....
+
+// Math Methods -
+// Math.abs(); 
+// Math.pow(); 
+// Math.floor();
+// Math.ceil();  
+// Math.random(); 
+
+// Math Methods -
+Math.abs(); // prints absolute number ie modulo --> prints always positive number of a negative number given or already positive given
+Math.abs(-5.5); // prints 5.5
+Math.abs(5.5); // prints 5.5
+
+
+Math.pow(); // gives value/ans of power to a number
+Math.pow(2,2); // prints 4 (2 to the power 2)
+Math.pow(2,3); // prints 8 (2 to the power 3)
+Math.pow(3,2); // prints 9 (3 to the power 2)
+
+Math.floor(); // rounds off value <= to number given (less than or equal to)
+Math.floor(5); // prints = value if whole number ie 5 
+Math.floor(5.5); // prints < value if decimal number ie 5
+Math.floor(-5.5); // prints < value if decimal number ie -6 so -6 < -5.5 
+// (for lower value of a negative number is the greater no. ie -6 < -5.5 < -5 )
+
+
+Math.ceil(); // rounds off value >= to number given (greater than or equal to)
+Math.ceil(5); // prints = value if whole number ie 5 
+Math.ceil(5.5); // prints > value if decimal number ie 6
+Math.ceil(-5.5); // prints > value if decimal number ie -5 so -5.5 < -5
+// (for greater value of a negative number is the lower no. ie -6 < -5.5 < -5)
+
+Math.random(); // generates any random number
