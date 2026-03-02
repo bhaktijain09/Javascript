053fee60a0e4e5edf29c7302e9dd97af00ef2378
ACM <223bhakti0079@dbit.in>
2026-03-03 00:02:04 +0530

    FunctionswithArguments

---
+++ JS6/FunctionswithArguments.js
@@ -0,0 +1,22 @@
+// Function with Arguments -
+
+// function functionName(argument1, argument2)
+// {
+//        do something - logic 
+// }
+// functionName(parameter1, parameter2);
+
+
+function info(name, age)  // function functionName(argument1, argument2)
+{
+console.log(`${name}'s age is ${age}!`)  // logic
+}
+info("Bhakti", 21); // functionName(parameter1, parameter2);
+info("Jai", 19); // functionName(parameter1, parameter2);
+
+
+// Arguments are the variable names listed in the function definition.
+// Parameters are the actual values you pass into the function when calling it. 
+// ie 
+// Arguments - variable names in function definition
+// Parameters - values passed in function calling.
