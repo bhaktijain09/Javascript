6133a436f6920ff8d727e60348f23a8c19c232c1
ACM <223bhakti0079@dbit.in>
2026-03-02 23:57:18 +0530

    QuestionsJS5

---
+++ JS5/QuestionsJS5.js
@@ -0,0 +1,11 @@
+// Generate any random number between 1 - 100 
+console.log(Math.floor(Math.random()*100) + 1); // so (*100 for range 0-99) and +1 for range 1 - 100
+
+// Generate any random number between 1 - 5 
+console.log(Math.floor(Math.random() * 5) + 1); // so (*5 for range 0-4) and +1 for range 1 - 5
+
+// Generate any random number between 21 - 25 
+console.log(Math.floor(Math.random() * 5) + 21); // so (*5 for range 0-4) and +21 for range 21 - 25
+
+// Generate any random number between 212 - 221 
+console.log(Math.floor(Math.random() * 10) + 212); // so (*5 for range 0-9) and +212 for range 212 - 221
